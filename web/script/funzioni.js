12dc33bacf83bcf9cdfb23c6bf99041526e2e959
pio-debug <francescop.covino@gmail.com>
2020-06-26 12:22:57 +0200

    pulsante per andare su

--- web/script/funzioni.js
+++ web/script/funzioni.js
@@ -5,3 +5,22 @@ function inF(obj) { obj.innerHTML = "favorite_border"; }
 function outF(obj) { obj.innerHTML = "favorite"; }
 function inA(obj) { obj.innerHTML = "account_circle"; }
 function outA(obj) { obj.innerHTML = "account_box"; }
+
+jQuery(document).ready(function() {
+
+    var btn = $('#goup');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
+
+});
